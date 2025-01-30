9f73c6088d7b65b92393404ce7f0ddf90a388586
Argus36 <artemsolomakha2007@gmail.com>
2025-01-30 23:50:52 +0300

    added time and studied useEffect a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import moment from "moment";
+import { useState, useEffect } from "react";
+
+
 
 
 function App() {
+
+  const [date, setDate] = useState(moment().format("dddd DD MMMM YYYY"));
+  const [time, setTime] = useState(moment().format("HH mm ss"));
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(moment().format("dddd DD MMMM YYYY"));
+      setTime(moment().format("HH:mm:ss"));
+    }, 500);
+    return () => clearInterval(interval)
+  }, []);
+
+
+
+
+
   return (
     <div className="App">
-
+      <h1>Сегодняшняя дата — {date}</h1>
+      <h1>Время — {time}</h1>
     </div>
   );
 }
+
 
 export default App;
