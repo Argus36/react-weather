5290db5517e8d8a7d602575191faddb9de0ec19e
Argus36 <artemsolomakha2007@gmail.com>
2025-02-14 22:35:37 +0300

    Next

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [windDirection, setWindDirection] = useState(null);
 
-
-
-
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
       setLat(position.coords.latitude);
@@ -27,8 +24,12 @@ function App() {
 
   const getWeatherData = async () => {
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&lang=ru`);
+      const pos = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${apiKey}`);
+      const posw = await pos.json();
+
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&q=${posw[0].name}&limit=5&units=metric&appid=${apiKey}&lang=ru`);
       const data = await response.json();
+
       setWeatherData(data);
     } catch (error) {
       console.log(error);
@@ -85,34 +86,35 @@ function sunS(date) {
 
 function background(back_weather) {
   switch (back_weather) {
-    case 'ясно':
+    case 'Clear':
       return (
-        {background: `linear-gradient(165deg, rgb(252, 201, 24) 0%, rgb(255, 255, 255) 40%, rgb(195, 241, 254) 80%)`}
+        {background: `linear-gradient(170deg, rgb(252, 201, 24) 0%, #B6D5DD 40%, #D1FEFE 82%)`}
       );
       break;
-    case 'небольшая облачность':
+    case 'Clouds':
       return (
-        {background: `linear-gradient(165deg, rgb(194, 194, 194) 0%, rgb(255, 255, 255) 40%, rgb(195, 241, 254) 80%)`}
+        {background: `linear-gradient(170deg, #D0D0E2 23%, #C2CCD1 59%, #B6C9DD 100%)`}
       );
       break;
-    case 'пасмурно':
+    case 'Drizzle' || 'Rain':
       return (
-        {background: `linear-gradient(165deg, rgb(63, 63, 63) 0%, rgb(255, 255, 255) 40%, rgb(195, 241, 254) 80%)`}
+        {background: `linear-gradient(170deg, rgb(152, 231, 255) 0%, rgb(255, 255, 255) 40%, rgb(195, 241, 254) 80%)`}
       );
       break;
-    case 'небольшой дождь':
+      case 'Thunderstorm':
       return (
-        {background: `linear-gradient(145deg, rgb(152, 231, 255) 0%, rgb(255, 255, 255) 40%, rgb(195, 241, 254) 80%)`}
+        //{background: `linear-gradient(170deg, #1A103B 8%, #1B1434 27%, #1D192B 49%, #1A1727 57%, #0F0F10 84%)`}
+        {background: `linear-gradient(170deg, #302E48 23%, #4B4A57 59%, #676767 100%)`}
       );
       break;
-    case 'снег':
+    case 'Show':
       return (
-        {background: `linear-gradient(165deg, rgb(25, 0, 255) 0%, rgb(255, 255, 255) 40%, rgb(195, 241, 254) 80%)`}
+        {background: `linear-gradient(170deg, #FFFFFF 40%,rgb(165, 165, 165) 80%`}
       );
       break;
     default:
       return (
-        {background: `linear-gradient(165deg, #BCE7F4 5%, #82C8DD 17%, #F6AB79 37%, #818CD2 61%, #252F72 79%, #11194E 93%)`}
+        {background: `linear-gradient(170deg, #BCE7F4 5%, #82C8DD 17%, #F6AB79 37%, #818CD2 61%, #252F72 79%, #11194E 93%)`}
       )
       break;
   }
@@ -121,44 +123,49 @@ function background(back_weather) {
   return (
     <div className="App">
         {!weatherData ? (
-          <div style={{background: `linear-gradient(165deg, #BCE7F4 5%, #82C8DD 17%, #F6AB79 37%, #818CD2 61%, #252F72 79%, #11194E 93%)`,
+          <div style={{background: `white`,
             display: `flex`,
             alignItems: `center`,
             justifyContent: `center`}} className="weater_container">
             <p style={{fontSize: `3vw`}}>Загрузка ...</p>
           </div>
         ) : (
-          <div style={{background: `linear-gradient(165deg, #BCE7F4 5%, #82C8DD 17%, #F6AB79 37%, #818CD2 61%, #252F72 79%, #11194E 93%)`}} className="weater_container">
-            <div className="phone">
-              <p>Широта : '{lat}'</p>
-              <p>Долгота : '{lon}'</p>
-              <p>Название города : '{weatherData.name}'</p>
-              <p>Фактическая температура : '{weatherData.main.temp}'</p>
-              <p>Как ощущается '{weatherData.main.feels_like}'</p>
-              <p>Сама погода : '{String(weatherData.weather[0].description).charAt(0).toUpperCase() + String(weatherData.weather[0].description).slice(1)}'</p>
-              <p>Облачность : '{weatherData.clouds.all}%'</p>
-              <p>Скорость ветра : '{weatherData.wind.speed}'</p>
-              <p>Направление : '{windeg(weatherData.wind.deg)}'</p>
-              <p>Атмосферное давление : '{aM(weatherData.main.grnd_level)} мм рт.ст'</p>
-              <p>Влажность : '{weatherData.main.humidity}%'</p>
-              <p>Время восхода солнца : {sunR(weatherData.sys.sunrise)}</p>
-              <p>Время захода солнца : {sunS(weatherData.sys.sunset)}</p>
+          <div>
+            <div style={{background: `linear-gradient(170deg, #FFFFFF 40%,rgb(165, 165, 165) 80%`}} className="weater_container">
+              <p style={{position: `fixed`, top: `2.5%`, left: `0`}}>Широта : '{lat}'</p>
+              <p style={{position: `fixed`, top: `5%`, left: `0`}}>Долгота : '{lon}'</p>
+              <div className="phone">
+                <div>
+                  <p>Название города : '{weatherData.name}'</p>
+                  <p>Фактическая температура : '{weatherData.main.temp}'</p>
+                  <p>Как ощущается '{weatherData.main.feels_like}'</p>
+                  <p>base '{weatherData.weather[0].main}'</p>
+                  <p>Сама погода : '{String(weatherData.weather[0].description).charAt(0).toUpperCase() + String(weatherData.weather[0].description).slice(1)}'</p>
+                  <p>Облачность : '{weatherData.clouds.all}%'</p>
+                  <p>Скорость ветра : '{weatherData.wind.speed}'</p>
+                  <p>Направление : '{windeg(weatherData.wind.deg)}'</p>
+                  <p>Атмосферное давление : '{aM(weatherData.main.grnd_level)} мм рт.ст'</p>
+                  <p>Влажность : '{weatherData.main.humidity}%'</p>
+                  <p>Время восхода солнца : {sunR(weatherData.sys.sunrise)}</p>
+                  <p>Время захода солнца : {sunS(weatherData.sys.sunset)}</p>
+                  <p>иконка : {weatherData.weather[0].icon}</p>
+                </div>
+              </div>
+            </div>
+            <div className="sun_container">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, dignissimos unde corporis et ullam quam cum dicta vel possimus reiciendis illo voluptatibus sed saepe molestias tenetur esse facilis, nemo libero?</h1>
             </div>
           </div>
         )}
-      <div className="sun_container">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, dignissimos unde corporis et ullam quam cum dicta vel possimus reiciendis illo voluptatibus sed saepe molestias tenetur esse facilis, nemo libero?</h1>
-      </div>
     </div>
   );
 }
 
 /*
-  <p>Широта : '{lat}'</p>
-  <p>Долгота : '{lon}'</p>
   <p>Название города : '{weatherData.name}'</p>
   <p>Фактическая температура : '{weatherData.main.temp}'</p>
   <p>Как ощущается '{weatherData.main.feels_like}'</p>
+  <p>base '{weatherData.weather[0].main}'</p>
   <p>Сама погода : '{String(weatherData.weather[0].description).charAt(0).toUpperCase() + String(weatherData.weather[0].description).slice(1)}'</p>
   <p>Облачность : '{weatherData.clouds.all}%'</p>
   <p>Скорость ветра : '{weatherData.wind.speed}'</p>
@@ -167,6 +174,7 @@ function background(back_weather) {
   <p>Влажность : '{weatherData.main.humidity}%'</p>
   <p>Время восхода солнца : {sunR(weatherData.sys.sunrise)}</p>
   <p>Время захода солнца : {sunS(weatherData.sys.sunset)}</p>
+  <p>иконка : {weatherData.weather[0].icon}</p>
 */
 
 
